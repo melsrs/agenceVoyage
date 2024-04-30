813e8e8b612a54dd20aa1698524df0d3406bf50c
Mélissa <melissa.soares1909@gmail.com>
2024-04-30 19:32:52 +0200

    ajout VoyageController et modif HomeController

--- backend/src/Controller/VoyageController.php
+++ backend/src/Controller/VoyageController.php
@@ -7,14 +7,14 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 
-
-class HomeController extends AbstractController
+#[Route('/voyage', name: 'app_voyage_')]
+class VoyageController extends AbstractController
 {
-    #[Route('/homepage', name: 'app_home')]
+    #[Route('s', name: 'index')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('voyage/index.html.twig', [
+            'controller_name' => 'VoyageController',
         ]);
     }
 }
