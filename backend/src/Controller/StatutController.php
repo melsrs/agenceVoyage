b2ca2727352614151b9eb09150fee1e6672ce415
Mélissa <melissa.soares1909@gmail.com>
2024-05-03 16:10:54 +0200

    image

---
+++ backend/src/Controller/StatutController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Statut;
+use App\Form\StatutType;
+use App\Repository\StatutRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted('ROLE_ADMIN')]
+#[Route('/statut', name:"app_statut_")]
+class StatutController extends AbstractController
+{
+    #[Route('s', name: 'index')]
+    public function index(StatutRepository $statutRepository): Response
+    {
+        return $this->render('statut/index.html.twig', [
+            'statuts' => $statutRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $statut = new Statut();
+        $form = $this->createForm(StatutType::class, $statut);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($statut);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_statut_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('statut/new.html.twig', [
+            'statut' => $statut,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'show', methods: ['GET'])]
+    public function show(Statut $statut): Response
+    {
+        return $this->render('statut/show.html.twig', [
+            'statut' => $statut,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Statut $statut, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(StatutType::class, $statut);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Le statut a été mise à jour');
+
+            return $this->redirectToRoute('app_statut_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('statut/edit.html.twig', [
+            'statut' => $statut,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'delete', methods: ['POST'])]
+    public function delete(Statut $statut, EntityManagerInterface $entityManager): Response
+    {
+            $entityManager->remove($statut);
+            $entityManager->flush();
+
+        $this->addFlash('success', 'Le statut a bien été supprimée');
+
+        return $this->redirectToRoute('app_statut_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
